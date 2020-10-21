83cc0efffdf727c6060ad41218cb4a43da3212a2
sathishsk12 <dummyaccsk@gmail.com>
2020-10-21 22:07:50 +0530

    Updates

---
+++ precache-manifest.8d3ceb7674730b31e40a53d89574972f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0346a9e8342a7092b47dca2f9ab27cd5",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "998cf9143ebbff4020e7",
+    "url": "/react-deploy/static/css/main.559a7b9d.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/react-deploy/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998cf9143ebbff4020e7",
+    "url": "/react-deploy/static/js/main.b6c9c913.chunk.js"
+  },
+  {
+    "revision": "b4688aaab1071dd34876",
+    "url": "/react-deploy/static/js/runtime-main.3fbfa92b.js"
+  },
+  {
+    "revision": "cf60ec56523044453a21d64618ebfbf1",
+    "url": "/react-deploy/static/media/Music App.cf60ec56.jpg"
+  },
+  {
+    "revision": "1c4908acf8cad36ca170cba5e5b6b773",
+    "url": "/react-deploy/static/media/dribble.1c4908ac.png"
+  },
+  {
+    "revision": "d19bbdb4c119b7d984414179907a58d1",
+    "url": "/react-deploy/static/media/github.d19bbdb4.png"
+  }
+]);
